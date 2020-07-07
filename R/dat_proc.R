5ae37a689af6e1a1c2d0af23b78a36f166fea402
fawda123 <mbafs2012@gmail.com>
2020-07-07 16:38:49 -0400

    messing with year reference selection plot

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,9 +1,19 @@
 library(tbeptools)
 library(tidyverse)
 library(here)
+library(extrafont)
+
+fml <- 'Lato Light'
 
 # tbni scores -------------------------------------------------------------
 
 tbniscr <- anlz_tbniscr(fimdata)
 
 save(tbniscr, file = here('data/tbniscr.RData'))
+
+# matrix results ----------------------------------------------------------
+
+tbniscr <- anlz_tbniscr(fimdata)
+tbnimat <- show_tbnimatrix(tbniscr, family = fml, plotly = TRUE, txtsz = NULL)
+
+save(tbnimat, file = here('data/tbnimat.RData'), compress = 'xz')
