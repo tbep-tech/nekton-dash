23763d88756b107d356326d983e49b129500ae76
fawda123 <mbafs2012@gmail.com>
2020-07-07 18:06:10 -0400

    gauge scores and metrics for map selection

---
+++ R/funcs.R
@@ -0,0 +1,13 @@
+# gauge function for raw data
+rawgaugefun <- function(fimplo, tbniscr, cols, colnm, ttl){
+  
+  qnts <- quantile(tbniscr[, colnm, drop = T], c(0, 0.33, 0.66, 0.95), na.rm = T) %>% round(1) %>%  as.numeric
+  
+  out <- gauge(round(fimplo[, colnm, drop = T], 1), min = qnts[1], max = qnts[4],label = ttl, gaugeSectors(
+    danger = qnts[c(1, 2)], warning = qnts[c(2, 3)], success = qnts[c(3, 4)],
+    colors = rev(cols)
+  ))
+  
+  return(out)
+  
+}
