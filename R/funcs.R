89ccca60c463377f81f9c78e04296a58ed27a254
fawda123 <mbafs2012@gmail.com>
2020-07-09 12:02:54 -0400

    modified gauges for switch on raw/scaled metrics and added percentile gauges

--- R/funcs.R
+++ R/funcs.R
@@ -1,13 +1,53 @@
 # gauge function for raw data
-rawgaugefun <- function(fimplo, tbniscr, cols, colnm, ttl){
+gaugefun <- function(fimplo, tbniscr, cols, colnm, ttl, raw = FALSE){
+
+  scrmet <- fimplo[, paste0('Score', colnm), drop = T]
+
+  if(!raw){
+    
+    out <- gauge(scrmet, min = 0, max = 10, label = ttl, gaugeSectors(
+      danger = c(0, 3), warning = c(3, 7), success = c(7, 10),
+      colors = rev(cols)
+    ))
+    
+  }
   
-  qnts <- quantile(tbniscr[, colnm, drop = T], c(0, 0.3, 0.7, 0.95), na.rm = T) %>% round(1) %>%  as.numeric
+   
+  if(raw){
+    
+    # get color from metric value, assign to raw for continuity
+    col <- findInterval(scrmet, c(0, 3, 7, 10), rightmost.closed = T)
+    col <- case_when(
+      col %in% c(0, 1) ~ cols[1], 
+      col == 2 ~ cols[2], 
+      col == 3 ~ cols[3]
+    )
+
+    qnts <- quantile(tbniscr[, colnm, drop = T], c(0, 0.95), na.rm = T) %>% round(1) %>%  as.numeric
+    
+    out <- gauge(round(fimplo[, colnm, drop = T], 1), min = qnts[1], max = qnts[2],label = ttl, gaugeSectors(colors = col))
+    
+  }
   
-  out <- gauge(round(fimplo[, colnm, drop = T], 1), min = qnts[1], max = qnts[4],label = ttl, gaugeSectors(
-    danger = qnts[c(1, 2)], warning = qnts[c(2, 3)], success = qnts[c(3, 4)],
-    colors = rev(cols)
-  ))
+  return(out)
   
+}
+
+# gauge function for percentile reporting
+gaugeqfun <- function(fimplo, fimyrs, colnm, ttl){
+  
+
+  scrmet <- fimplo[, colnm, drop = T]
+  
+  # get percentile
+  ptile <- ecdf(fimyrs[, colnm, drop = T])(scrmet)
+  ptile <- round(100 * ptile, 0)
+  
+  out <- gauge(ptile, min = 0, max = 100, label = ttl, gaugeSectors(
+    danger = c(0, 33), warning = c(33, 66), success = c(66, 100),
+    colors = c("#312271", "#A79FE1", "#F9F9F9")
+    ), symbol = '%')
+
   return(out)
   
 }
